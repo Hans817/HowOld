0a17d0257aa7e60d43438b7b5f6932bbb1a749ec
Jihan <958302188@qq.com>
2016-08-08 10:20:14 +0800

    the howold

---
+++ app/src/main/java/com/jihan/howold/Constant.java
@@ -0,0 +1,10 @@
+package com.jihan.howold;
+
+/**
+ * Created by Jim Huang on 2016.08.01.
+ */
+public class Constant {
+    public static final String KEY="f6cf4ec064f232ff09f2b41921e2f6e3";
+    public static final String SECRET="h_909cEjrCZlcxGT8QP4KaHsggJ6Jts5 ";
+
+}
